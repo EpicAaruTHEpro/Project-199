6c75ed0396fc5fe0783c04df8577627e73b35915
Aarush Noheria <atomicoolives@gmail.com>
2023-08-06 22:05:38 -0400

    Fixed quiz

--- quiz_server.py
+++ quiz_server.py
@@ -8,8 +8,8 @@ server.bind((ipaddress, port))
 server.listen()
 clients = []
 nicknames = []
-questions = ["What is the largest state in the USA? \n a.Texas \n b.California \n c.Alaska \n d.Washington"]
-answers = ["c"]
+questions = ["What is the largest state in the USA? \n a.Texas \n b.California \n c.Alaska \n d.Washington", "Which major aircraft manufacturer unveiled its lighter, smoother, more turbulent-resistant 787 Dreamliner in 2007? \n a. Airbus \n b. Northrop Grumman \n c. General Electric \n d. Boeing", "How many sides does a stop sign have? \n a. 5 \n b. 6 \n c. 7 \n d. 8"]
+answers = ["c","d","d"]
 print("Server is running")
 def broadcast(message, connection): 
     for client in clients:
@@ -44,19 +44,20 @@ def clientThread(conn, nickname) :
     conn.send("Welcome to this quiz game".encode("utf-8"))
     conn.send("Answers to questions are either a, b, c, or d".encode("utf-8"))
     conn.send("Good Luck!!\n\n".encode("utf-8"))
-    index,question,answer = get_random_question_answer(conn)
+    index,question,answer = get_random_question_answer()
     while True:
         try:
             message = conn.recv(2048).decode("utf-8")
             if (message) :
-                if message.lower() == answer:
+                print("Message:", message)
+                if message.lower().split(":")[1] == answer:
                     score+=1
                     conn.send(f"Bravo your score is {score}\n\n".encode("utf-8"))
 
                 else:
                    conn.send(f"Wrong!! Better luck next time!\n\n".encode("utf-8")) 
                 remove_question(index)
-                index,question,answer = get_random_question_answer(conn)
+                index,question,answer = get_random_question_answer()
             else: 
                 remove(conn)
                 remove_nickname(nickname)
